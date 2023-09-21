e6811ff0eafac3e8dd2e48752d3ffdd3a1bf4738
trang051201 <trangtrau201@gmail.com>
2023-09-21 17:52:22 +0700

    firstcommit

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿﻿
 Console.WriteLine("Hello, World!");
+//ví dụ
+//in ra màn hình: 6
+Console.WriteLine(6);
+// in ra màn hình : a = 9
+int a = 9; //khai bao biến số a kiểu int vfaf gán giá trị = 9
+Console.WriteLine("a = " + a);
+//in ra màn hình a + b = 12
+int a1 = 9;
+int b1 = 3;
+Console.WriteLine("{0} + {1} = {2}", a1, b1, a1+b1);
