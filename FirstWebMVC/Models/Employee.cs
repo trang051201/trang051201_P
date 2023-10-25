2b3c7dec6e86e1791d8b1f7fb1b82a9cd922a6a0
trang051201 <100513953+trang051201@users.noreply.github.com>
2023-10-25 19:37:18 +0700

    Add files via upload

---
+++ FirstWebMVC/Models/Employee.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+//Nghiem Thi Van Anh -2021050078
+namespace DemoMVC.Models
+{
+    public class Employee 
+    {
+        public string FullName { get; set; }
+        public int HeSoLuong { get; set; } 
+        public int LuongCB { get; set; }
+        public int PhuCap { get; set; }
+    //Nghiem Thi Van Anh -2021050078
+    }
+}
