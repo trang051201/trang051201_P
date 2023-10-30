3990e4c1edf7176a346757accfa406fd7ca63255
trang051201 <trangtrau201@gmail.com>
2023-10-31 00:27:12 +0700

    Giai Phuong Trinh Bac Nhat- MVC

--- DemoMVC/Controllers/PhuongTrinhBacNhat.cs
+++ DemoMVC/Controllers/PhuongTrinhBacNhat.cs
@@ -7,7 +7,7 @@ namespace DemoMVC.Controllers
         {
             return View();
         }
-//Quách kiều trang - 2021050654
+//Quách kiều trang - 2021050654. 
 //Giai phuong trinh bac nhat
         [HttpPost]
         public IActionResult Index(double a, double b)
